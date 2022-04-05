0c8d0d44826a2415bca7f9ed46fe79a5d9c8fcc1
aampinar <98187277+aampinar@users.noreply.github.com>
2022-04-05 12:52:03 -0400

    Add OMD

--- site/index.js
+++ site/index.js
@@ -1,5 +1,7 @@
 const url = "https://swapi.dev/api/films/"
+const url1 = "http://img.omdbapi.com/?apikey=[3a0a823c]&"
 const main = document.querySelector("main")
+const revenge = "http://www.omdbapi.com/?t=revenge+of+the+sith&apikey=3a0a823c"
 
 
 function addMoviesListings(movie) {
@@ -12,7 +14,6 @@ function addMoviesListings(movie) {
     main.append(div)
 }
 
-
 fetch(url)
     .then(response => {
         return response.json()
@@ -21,7 +22,20 @@ fetch(url)
         const fetches = urls.map(url => fetch(url).then(response => response.json()))
         return Promise.all(fetches)
     }).then(responses => {
-            responses.forEach(response => {
+        responses.forEach(response => {
+            addMoviesListings(response)
+        })
+    })
+
+fetch(url1)
+    .then(response => {
+        return response.json()
+    }).then(parsedResponse => {
+        const urls = parsedResponse.results.map(result => result.url1)
+        const fetches = urls.map(url => fetch(url).then(response => response.json()))
+        return Promise.all(fetches)
+    }).then(responses => {
+        responses.forEach(response => {
             addMoviesListings(response)
         })
     })
