803bd09be77b23833937231b7405521257650e84
aampinar <98187277+aampinar@users.noreply.github.com>
2022-04-05 15:08:59 -0400

    Add movie photos

--- site/index.js
+++ site/index.js
@@ -1,8 +1,12 @@
 const url = "https://swapi.dev/api/films/"
-const url1 = "http://img.omdbapi.com/?apikey=[3a0a823c]&"
 const main = document.querySelector("main")
-const revenge = "http://www.omdbapi.com/?t=revenge+of+the+sith&apikey=3a0a823c"
-
+const ul = document.querySelector('ul')
+const movieNewHope = document.createElement('img')
+const movieStrikes = document.createElement('img')
+const movieReturn = document.createElement('img')
+const moviePhantom = document.createElement('img')
+const movieClones = document.createElement('img')
+const movieRevenge = document.createElement('img')
 
 function addMoviesListings(movie) {
     const div = document.createElement("div")
@@ -27,15 +31,18 @@ fetch(url)
         })
     })
 
-fetch(url1)
-    .then(response => {
-        return response.json()
-    }).then(parsedResponse => {
-        const urls = parsedResponse.results.map(result => result.url1)
-        const fetches = urls.map(url => fetch(url).then(response => response.json()))
-        return Promise.all(fetches)
-    }).then(responses => {
-        responses.forEach(response => {
-            addMoviesListings(response)
-        })
-    })
+
+
+movieNewHope.src = 'https://m.media-amazon.com/images/I/81RZipc6yOL._AC_SY879_.jpg'
+movieStrikes.src = 'https://starwarsblog.starwars.com/wp-content/uploads/2020/06/the-empire-strikes-back-post-m-ferguson.jpg'
+movieReturn.src = "https://cdn.shopify.com/s/files/1/0057/3728/3618/products/f38cb38707f05dc86f483b07c215c7dd_a1fb33f9-f075-438f-bc49-2e8c2a2e2d02_500x.jpg?v=1573653657"
+moviePhantom.src = "https://m.media-amazon.com/images/M/MV5BYTRhNjcwNWQtMGJmMi00NmQyLWE2YzItODVmMTdjNWI0ZDA2XkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_.jpg"
+movieClones.src = "https://picfiles.alphacoders.com/124/124911.jpg"
+movieRevenge.src = "https://m.media-amazon.com/images/M/MV5BNTc4MTc3NTQ5OF5BMl5BanBnXkFtZTcwOTg0NjI4NA@@._V1_.jpg"
+
+ul.append(movieNewHope)
+ul.append(movieStrikes)
+ul.append(movieReturn)
+ul.append(moviePhantom)
+ul.append(movieClones)
+ul.append(movieRevenge)
